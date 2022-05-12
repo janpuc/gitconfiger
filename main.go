a8c60f1235218f6af88f7cb439e040f4e6f59d88
Jan Pucilowski <jan.pucilowski99@gmail.com>
2022-05-12 14:49:20 +0200

    Added create file function

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	createFile("test")
+}
+
+func createFile(name string) {
+	file, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File created")
+	defer file.Close()
 }
